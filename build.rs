1ba34b00d3048cc34408c747fa4c51cde9d3bc0c
lumpyh <lumpy.hega@gmx.de>
2025-03-08 12:40:58 +0100

    add build rs

---
+++ build.rs
@@ -0,0 +1,13 @@
+use std::{env, path::PathBuf};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+
+    tonic_build::configure()
+        .file_descriptor_set_path(out_dir.join("tournament_descriptor.bin"))
+        .compile_protos(
+            &["tournament_protos/tournament.proto"],
+            &["tournament_protos"],
+        )?;
+    Ok(())
+}
